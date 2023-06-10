f9fbbe04726d0cbb6395535fb577d8ada30c0041
MishinIgor <136114259+MishinIgor@users.noreply.github.com>
2023-06-10 10:50:05 +0500

    Практическая работа №5

---
+++ PR5.1.py
@@ -0,0 +1,8 @@
+a = input("Введите строку без пробелов: ")
+k = ''.join(reversed(a))
+if k == a:
+    print('yes')
+else:
+    print('no')
+#https://github.com/MishinIgor/Pract1.git
+#PR5.1
---
+++ PR5.2.py
@@ -0,0 +1,6 @@
+a = input("Введите строку: ")
+k = a.split()
+for i in k:
+    print(i, end=" ")
+#https://github.com/MishinIgor/Pract1.git
+#PR5.2
